f9883b4e71abc76f6d73020316d2860c554034c6
DailyProgress1 <chhotusidhant@gmail.com>
2025-07-09 08:44:11 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -23,9 +23,25 @@
 // });
 
 
-res.cookie("jwt", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // ✔️ production me true
-  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✔️ for cross-site
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-});
+// res.cookie("jwt", token, {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production", // ✔️ production me true
+//   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✔️ for cross-site
+//   maxAge: 7 * 24 * 60 * 60 * 1000,
+// });
+
+
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
